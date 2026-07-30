585e3dcd97a12fdc8afaec4021234230d0c7c03e
YanSP <yandasilvapires@gmail.com>
2026-07-30 16:48:58 -0300

    Atualiza explicação sobre float

---
+++ explicando_float.py
@@ -0,0 +1,23 @@
+# Explicando o tipo float
+
+#Errado
+valor = 1, 44
+
+print(valor)
+print(type(valor))
+print('Formatação incorreta do número decimal\n')
+
+#Certo
+valor = 1.44
+
+print(valor)
+print(type(valor))
+print('Formatação correta do número decimal\n')
+
+#É possível
+valor1, valor2 = 1, 44
+print(valor1)
+print(type(valor1))
+print(valor2)
+print(type(valor2))
+print('É possível atribuir múltiplos valores a múltiplas variáveis, mas não é a forma correta de representar um número decimal\n')
